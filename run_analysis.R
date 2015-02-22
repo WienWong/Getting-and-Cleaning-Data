494aad343dc1b79ba1ecf5000e60d85bef8da144
WienWong <wangzichengfu@gmail.com>
2015-02-22 18:21:10 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 
 library(reshape2)
-setwd("D:/Coursera_R/GC_Proj")
+setwd("D:/Coursera_R/GC_Proj") # I put train, test, features.txt etc under this 'GC_Proj' folder.
 
 ## Read features
 features  <- read.table('features.txt')[, 2] 
 features
 
-ft        <- grepl("std | mean", features) # grepl returns TRUE if a string contains the pattern, otherwise FALSE; 
+ft <- grepl("std | mean", features) 
 ft
 
 activities<- read.table('activity_labels.txt',col.names = c('Code', 'ActivityName'))
@@ -23,11 +23,11 @@ ytrain    <- read.table("train/y_train.txt", col.names = c('Activity'))
 subjtrain <- read.table("train/subject_train.txt", col.names = c('Subject'))
 
 ## Merge test and data sets
-xys       <- cbind(rbind(ytest, ytrain), rbind(subjtest, subjtrain), rbind(xtest, xtrain))
-xys       <- merge(x = xys, y = activities, by.x = 'Activity', by.y = 'Code', all = TRUE)[, -1]
+xysbj     <- cbind(rbind(ytest, ytrain), rbind(subjtest, subjtrain), rbind(xtest, xtrain))
+xysbj     <- merge(x = xysbj, y = activities, by.x = 'Activity', by.y = 'Code', all = TRUE)[, -1]
 
 ## Create tidy dataset
-meltDt    <- melt(xys, id.vars = c("Subject", 'ActivityName'))
+meltDt    <- melt(xysbj, id.vars = c("Subject", 'ActivityName'))
 tidyDt    <- dcast(meltDt, Subject + ActivityName ~ variable, mean)
 
 # write tiny dataset
